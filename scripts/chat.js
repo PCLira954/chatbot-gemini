6a45b19e7a7df73dfbae91de5237648c78b6691a
PCLira954 <126837594+PCLira954@users.noreply.github.com>
2025-07-08 20:53:59 -0300

    Update chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -54,7 +54,7 @@ sendBtn.addEventListener('click', async () => {
     };
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=" + API_KEY,
+      "https://generativelanguage.googleapis.com/v1/models/gemini-pro-vision:generateContent?key=" + API_KEY,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
